0370d07f78a9c5f195616255e09668f1e270e07d
BiYuqi <biyuqiwan@163.com>
2019-12-10 01:21:51 +0800

    docs: update readme

--- README.md
+++ README.md
@@ -1,3 +1,69 @@
-# Mdppt
----
-A framework for easily creating beautiful presentations using Markdown.
+# Welcome to MDPPT(Still in coding, not release yet!) 👋
+![0.0.0.](https://img.shields.io/badge/version-0.0.0-blue.svg?cacheSeconds=2592000)
+[![Documentation](https://img.shields.io/badge/documentation-yes-brightgreen.svg)]( )
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://github.com/ftbjs/mdppt/blob/master/LICENSE)
+
+> A framework for easily creating beautiful presentations using Markdown.
+
+## [Homepage](https://github.com/ftbjs/mdppt)
+
+
+## Install
+
+```sh
+npm i mdppt -g
+```
+
+## Usage
+
+```sh
+ mdppt serve <filename>
+
+ mdppt build <filename>
+```
+## Run in local
+```sh
+git clone https://github.com/ftbjs/mdppt.git
+
+cd mdppt
+
+npm install && npm run install:dev
+
+cd packages/mdppt
+
+npm link
+
+# execute all above steps
+# you can run below command in any command line
+
+mdppt serve <filename>
+# or
+mdppt build <filename>
+```
+Note: the file should be allow markdown file.[e.g. DEMO](https://raw.githubusercontent.com/ftbjs/mdppt/master/mdppt.md)
+
+## Author
+
+👤 **BiYuqi**
+
+* Website: https://loadingmore.com
+* Github: [@BiYuqi](https://github.com/BiYuqi)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check [issues page](https://github.com/ftbjs/mdppt/issues).
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+[Give a star](https://github.com/ftbjs/mdppt/stargazers)
+
+
+## 📝 License
+
+Copyright © 2019 [BiYuqi](https://github.com/BiYuqi).
+
+This project is [MIT](https://github.com/ftbjs/mdppt/blob/master/LICENSE) licensed.
--- packages/mdppt/command/app.js
+++ packages/mdppt/command/app.js
@@ -2,62 +2,20 @@ const fs = require('fs-extra')
 const chalk = require('chalk')
 const inquirer = require('inquirer')
 const shelljs = require('shelljs')
-const { npmPackageNameRule, upperCamelCase } = require('../utils/helper')
 
+// TBD
 const app = async api => {
   return new Promise((resolve, reject) => {
     inquirer
       .prompt([
         {
-          name: 'appName',
-          message: 'Please input the project folder name:',
-          validate: appname => {
-            appname = appname.split(/\s/)
-            const getAppName = appname[0]
-            const getForceCreate = appname[1]
-            if (fs.existsSync(`./${getAppName}`)) {
-              if (getForceCreate && getForceCreate == '-f') {
-                shelljs.rm('-rf', `./${getAppName}`)
-                return true
-              } else {
-                return `Directory is exists, if you want to delete it, you can input: ${chalk.green(
-                  getAppName + ' -f'
-                )}`
-              }
-            } else {
-              return true
-            }
-          }
-        },
-        {
-          name: 'packageName',
-          message: 'Please input your package name:',
-          validate: name => {
-            if (!npmPackageNameRule.test(name)) {
-              return `Invalid package name.`
-            }
-            return true
-          }
-        },
-        {
-          type: 'list',
-          name: 'jest',
-          message: 'Do you want to add jest to test your code?',
-          choices: ['Yes', 'No']
-        },
-        {
-          type: 'list',
-          name: 'eslint',
-          message: 'Do you want to add eslint and prettier to format your code?',
+          type: 'name',
+          name: 'name',
+          message: 'what\'s the name?',
           choices: ['Yes', 'No']
         }
       ])
       .then(answer => {
-        // ignore space
-        api.appName = answer.appName.split(/\s/)[0]
-        api.packageName = upperCamelCase(answer.packageName)
-        api.jest = answer.jest === 'Yes'
-        api.eslint = answer.eslint === 'Yes'
         resolve()
       })
   })
